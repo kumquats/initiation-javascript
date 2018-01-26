a5958ae11f2f5b46ed413a80a091e8bc0d3e2cb4
thomas <tf@kumquats.fr>
2018-01-26 11:07:34 +0100

    01 : demarrage main.js

---
+++ 01-rappels-js/demarrage/js/main.js
@@ -0,0 +1,19 @@
+var file = 'catlaugh.jpg';
+
+var videos = [
+	{
+		title: 'CATS will make you LAUGH YOUR HEAD OFF - Funny CAT compilation',
+		thumbnail: 'catlaugh.jpg',
+		video: 'videos/catfails.mp4'
+	},
+	{
+		title: 'Hilarious Cat Fails (April 2017)',
+		thumbnail: 'catfails.jpg',
+		video: 'videos/catfails.mp4'
+	},
+	{
+		title: 'Cats are so funny you will die laughing - Funny cat compilation',
+		thumbnail: 'catssofunny.jpg',
+		video: 'videos/catfails.mp4'
+	}
+];
